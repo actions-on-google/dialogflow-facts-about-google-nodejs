69f03f70c5fab4510e9257610e4067fecd65f30d
Sachit Mishra <sachitm@google.com>
2016-11-21 20:13:21 -0800

    Initial checkin of Google Facts
    Initial checkin of Google Facts APIAI zip and App Engine app
    
    Change-Id: I4b850dfbf7b4c38907cc9953214493f7ae214813

---
+++ README.md
@@ -0,0 +1,49 @@
+# Actions On Google Google Facts Sample for Node.js
+==================================================
+
+This sample shows how to create an action for the Google Assistant using API.AI
+
+
+## Setup Instructions
+
+# Pre-requisites
+ 1. API.AI: [https://api.ai](https://api.ai)
+
+See the developer guide and release notes at https://developers.google.com/actions/ for more details.
+
+# Steps:
+1. Create a new API.AI agent at http://api.ai
+1. In your agent settings, navigate to Export and Import
+1. Click Import from Zip
+1. Choose the GoogleFacts.zip, included in this repository
+1. Follow the directions to import
+1. See the new intents in your project
+1. In the ```quit``` intent, mark End Conversation for Actions on Google
+1. Make sure all domains are turned off
+1. Deploy the app to your preferred hosting environment (we recommend Google App Engine)
+1. In the Fulfillment page, enable Fulfillment and specify the deployment URL
+1. In the Headers section, add a key for "Google-Assistant-Signature", with your own private key value
+1. In your Actions on Google integration, set ```start_facts``` as the start intent
+1. For the following intents, make sure fulfillment is enabled
+    * choose_cats
+    * choose_google
+    * say_cat_fact
+    * say_google_fact
+
+## Documentation
+* Actions on Google: https://developers.google.com/actions/
+
+## References and How to report bugs
+* If you find any issues, please open a bug here on GitHub
+
+How to make contributions?
+Please read and follow the steps in the CONTRIBUTING.md
+
+License
+See LICENSE.md
+
+## Terms
+Your use of this sample is subject to, and by using or downloading the sample files you agree to comply with, the [Google APIs Terms of Service](https://developers.google.com/terms/) and the [Actions On Google Developer Terms of Service](https://developers.google.com/actions/docs/terms/).
+
+## Google+
+Actions on Google Developers Community on Google+ [http://g.co/actionsdev](http://g.co/actionsdev)
---
+++ app.js
@@ -0,0 +1,204 @@
+// Copyright 2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+let Assistant = require('actions-on-google').ApiAiAssistant;
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+app.use(bodyParser.json({type: 'application/json'}));
+
+const GOOGLE_HEADER_SIGNATURE = 'Google-Assistant-Signature';
+const PRIVATE_KEY = 'YOUR_PRIVATE_KEY';
+
+const SAY_CAT_FACT = 'say_cat_fact';
+const SAY_GOOGLE_FACT = 'say_google_fact';
+const CATEGORY_ARGUMENT = 'category';
+const GOOGLE_CONTEXT = 'google-facts';
+const CAT_CONTEXT = 'cat-facts';
+const DEFAULT_LIFESPAN = 5;
+const END_LIFESPAN = 0;
+
+const FACT_TYPE = {
+  HISTORY: 'history',
+  HEADQUARTERS: 'headquarters',
+  CATS: 'cats'
+};
+
+const HISTORY_FACTS = new Set([
+  'Google was founded in 1998.',
+  'Google was founded by Larry Page and Sergey Brin.',
+  'Google went public in 2004.',
+  'Google has more than 70 offices in more than 40 countries.'
+]);
+
+const HQ_FACTS = new Set([
+  'Google\'s headquarters is in Mountain View, California.',
+  'Google has over 30 cafeterias in its main campus.',
+  'Google has over 10 fitness facilities in its main campus.'
+]);
+
+const CAT_FACTS = new Set([
+  'Cats are animals.',
+  'Cats have nine lives.',
+  'Cats descend from other cats.'
+]);
+
+const NEXT_FACT_DIRECTIVE = ' Would you like to hear another fact?';
+
+// This sample uses this sound from Freesound:
+// 'cat meow' by tuberatanka (https://www.freesound.org/people/tuberatanka/sounds/110011/)
+const MEOW_SRC = 'https://freesound.org/data/previews/110/110011_1537422-lq.mp3';
+
+function getRandomFact (facts) {
+  if (facts.size <= 0) {
+    return null;
+  }
+
+  let randomIndex = (Math.random() * (facts.size - 1)).toFixed();
+  let randomFactIndex = parseInt(randomIndex, 10);
+  let counter = 0;
+  let randomFact = '';
+  for (let fact of facts.values()) {
+    if (counter === randomFactIndex) {
+      randomFact = fact;
+      break;
+    }
+    counter++;
+  }
+  facts.delete(randomFact);
+  return randomFact;
+}
+
+// [START google_facts]
+app.post('/', function (req, res) {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  if (!assistant.isRequestFromApiAi(GOOGLE_HEADER_SIGNATURE, PRIVATE_KEY)) {
+    console.log('Request is not from trusted source (API.AI)');
+    return;
+  }
+
+  // Say a Google fact
+  function tellGoogleFact (assistant) {
+    let historyFacts = assistant.data.historyFacts
+      ? new Set(assistant.data.historyFacts) : HISTORY_FACTS;
+    let hqFacts = assistant.data.hqFacts
+      ? new Set(assistant.data.hqFacts) : HQ_FACTS;
+
+    if (historyFacts.size === 0 && hqFacts.size === 0) {
+      assistant.tell('Actually it looks like you heard it all. ' +
+        'Thanks for listening!');
+      return;
+    }
+
+    let factCategory = assistant.getArgument(CATEGORY_ARGUMENT);
+
+    if (factCategory === FACT_TYPE.HISTORY) {
+      let fact = getRandomFact(historyFacts);
+      if (fact === null) {
+        assistant.ask(noFactsLeft(assistant, factCategory,
+            FACT_TYPE.HEADQUARTERS));
+        return;
+      }
+
+      let factPrefix = 'Sure, here\'s a history fact. ';
+      assistant.data.historyFacts = Array.from(historyFacts);
+      assistant.ask(factPrefix + fact + NEXT_FACT_DIRECTIVE);
+      return;
+    } else if (factCategory === FACT_TYPE.HEADQUARTERS) {
+      let fact = getRandomFact(hqFacts);
+      if (fact === null) {
+        assistant.ask(noFactsLeft(assistant, factCategory,
+            FACT_TYPE.HISTORY));
+        return;
+      }
+
+      let factPrefix = 'Okay, here\'s a headquarters fact. ';
+      assistant.data.hqFacts = Array.from(hqFacts);
+      assistant.ask(factPrefix + fact + NEXT_FACT_DIRECTIVE);
+      return;
+    } else {
+      // Conversation repair is handled in API.AI, but this is a safeguard
+      assistant.ask('Sorry, I didn\'t understand. I can tell you about ' +
+        'Google\'s history, or its headquarters. Which one do you want to ' +
+        'hear about?');
+    }
+  }
+
+  // Say a cat fact
+  function tellCatFact (assistant) {
+    let catFacts = assistant.data.catFacts
+        ? new Set(assistant.data.catFacts) : CAT_FACTS;
+    let fact = getRandomFact(catFacts);
+    if (fact === null) {
+      let parameters = {};
+      // Add google-facts context to outgoing context list
+      assistant.setContext(GOOGLE_CONTEXT, DEFAULT_LIFESPAN,
+        parameters);
+      // Replace outgoing cat-facts context with lifespan = 0 to end it
+      assistant.setContext(CAT_CONTEXT, END_LIFESPAN, {});
+      assistant.ask('Looks like you\'ve heard all there is to know ' +
+        'about cats. Would you like to hear about Google?');
+      return;
+    }
+
+    let factPrefix = 'Alright, here\'s a cat fact. ' +
+      '<audio src="' + MEOW_SRC + '"></audio>';
+    let factSpeech = '<speak>' + factPrefix + fact +
+      NEXT_FACT_DIRECTIVE + '</speak>';
+    assistant.data.catFacts = Array.from(catFacts);
+    assistant.ask(factSpeech);
+    return;
+  }
+
+  // Say they've heard it all about this category
+  function noFactsLeft (assistant, currentCategory, redirectCategory) {
+    let parameters = {};
+    parameters[CATEGORY_ARGUMENT] = redirectCategory;
+    // Replace the outgoing google-facts context with different parameters
+    assistant.setContext(GOOGLE_CONTEXT, DEFAULT_LIFESPAN,
+        parameters);
+    let response = `Looks like you\'ve heard all there is to know\
+        about the ${currentCategory} of Google. Would you like to hear\
+        about its ${redirectCategory}? `;
+    if (!assistant.data.catFacts || assistant.data.catFacts.length > 0) {
+      response += 'By the way, I can tell you about cats too.';
+    }
+    return response;
+  }
+
+  let actionMap = new Map();
+  actionMap.set(SAY_GOOGLE_FACT, tellGoogleFact);
+  actionMap.set(SAY_CAT_FACT, tellCatFact);
+
+  assistant.handleRequest(actionMap);
+});
+// [END google_facts]
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  let server = app.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
+  // [END server]
+}
+
+module.exports = app;
